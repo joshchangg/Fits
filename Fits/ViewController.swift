1962704aab988dd3fd416b5190e20984e2cb0f7a
Joshua Chang <joshchangg01@gmail.com>
2021-12-04 23:38:52 -0500

    Initial Commit

---
+++ Fits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fits
+//
+//  Created by Joshua Chang  on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
